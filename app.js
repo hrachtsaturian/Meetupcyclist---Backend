e110afb1727fa7b3d07ebef9841db4e3b9ad23d8
Grachik Tsatsuryan <hrachtsaturian@gmail.com>
2024-12-17 22:32:49 -0800

    ensure cors

--- app.js
+++ app.js
@@ -17,16 +17,16 @@ const uploadImage = require("./routes/uploadImage");
 
 const app = express();
 
-app.use(express.json());
-app.use(authenticateJWT);
-
 // policies
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Origin", "https://meetupcyclist.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
+app.use(express.json());
+app.use(authenticateJWT);
 
 app.use("/", authRoutes);
 app.use("/users", usersRoutes);
